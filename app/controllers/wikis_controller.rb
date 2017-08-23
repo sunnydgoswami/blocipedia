9a2707d24ac469285884995871009c06e5f7a80b
SunnyGoswami <sunnydgoswami@gmail.com>
2017-08-23 20:02:20 +0000

    delete button added and working

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -43,4 +43,16 @@ class WikisController < ApplicationController
       render :edit
     end
   end
+  
+  def destroy
+    @wiki = Wiki.find(params[:id])
+    
+    if @wiki.destroy
+        flash[:notice] = "\"#{@wiki.title}\" was deleted successfully."
+        redirect_to wikis_path
+    else
+        flash.now[:alert] = "There was an error deleting the post."
+        render :show
+    end
+  end
 end
