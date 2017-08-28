ca0e71468c5cda6a3e6a79171b2e7359336c3c7c
SunnyGoswami <sunnydgoswami@gmail.com>
2017-08-28 21:16:15 +0000

    trying to get confirmation with heroku to work. You are close figure out why you acnnot find the right route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registration: 'registrations' }
+  devise_for :users
   
   devise_scope :user do
     get '/downgrade' => 'registrations#downgrade'
